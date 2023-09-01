7500789c22940026dbdc92707f211938563b5627
Rahul93729 <rahul.chalwadi@spit.ac.in>
2023-09-01 18:56:10 +0530

    abc

---
+++ models/pizzaModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const pizzaSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    varients: [],
+    prices: [],
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    Ingredient: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const pizzaModel = mongoose.model("pizza", pizzaSchema);
+module.exports = pizzaModel;
